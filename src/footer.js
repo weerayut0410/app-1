28c313f04dc47a948ac1c7d9ec5a9e2283769cdc
weerayut0410 <weerayut.la.65@ubu.ac.th>
2023-06-26 22:46:40 +0700

    first commit

---
+++ src/footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer(){
+    return (
+        <div className='MyFooter'>
+            <span>copylight 2023  @weerayut laksin</span>
+        </div>
+    );
+    }
+
+export default Footer;
